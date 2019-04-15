6b0f5b3c461acba196b3dd6e3d68bd14200420e0
Will Zawilinski <will.zawilinski@gmail.com>
2019-04-15 18:39:27 -0400

    Created DB Schema for housing experience review post, as well as some frontend boiler plate. Also created landing page, with non-functional nav-bar, and footer

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+class Landing extends Component {
+  render() {
+    return (
+      <div className="landing">
+        <div className="dark-overlay landing-inner text-light">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12 text-center">
+                <h1 className="display-3 mb-4">LiveOut</h1>
+                <p className="lead">
+                  {" "}
+                  Create a member profile and share your off-campus housing
+                  experiences with other members of the community.
+                </p>
+                <hr />
+                <a href="register.html" className="btn btn-lg btn-info mr-2">
+                  Sign Up
+                </a>
+                <a href="login.html" className="btn btn-lg btn-light">
+                  Login
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Landing;
