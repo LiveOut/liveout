881dc1d7307a384ea13ec5aa1717294624de95a4
Will Zawilinski <will.zawilinski@gmail.com>
2019-04-14 23:40:38 -0400

    Added profile add/edit routes as well as input validation for those fields

---
+++ validation/profile.js
@@ -0,0 +1,24 @@
+const isEmpty = require("./is_empty");
+const Validator = require("validator");
+
+module.exports = function validateProfileInput(data) {
+  let errors = {};
+
+  data.handle = !isEmpty(data.handle) ? data.handle : "";
+  data.status = !isEmpty(data.status) ? data.status : "";
+
+  if (!Validator.isLength(data.handle, { min: 2, max: 40 })) {
+    errors.handle = "Handle must be between 2 and 40 characters";
+  }
+  if (Validator.isEmpty(data.handle)) {
+    errors.handle = "Profile handle is required";
+  }
+
+  if (Validator.isEmpty(data.status)) {
+    errors.status = "Status field is required";
+  }
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
